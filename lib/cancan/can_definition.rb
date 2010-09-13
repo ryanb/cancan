e2c341b8d34051ac0d52464b9fc16649119e7f17
Sokolov Yura <funny.falcon@gmail.com>
2010-09-13 22:02:30 +0800

    allow to check ability by common module

--- lib/cancan/can_definition.rb
+++ lib/cancan/can_definition.rb
@@ -91,7 +91,7 @@ module CanCan
     end
 
     def matches_subject_class?(subject)
-      @subjects.any? { |sub| sub.kind_of?(Class) && (subject.kind_of?(sub) || subject.kind_of?(Class) && subject.ancestors.include?(sub)) }
+      @subjects.any? { |sub| sub.kind_of?(Module) && (subject.kind_of?(sub) || subject.kind_of?(Module) && subject.ancestors.include?(sub)) }
     end
 
     def matches_conditions_hash?(subject, conditions = @conditions)
