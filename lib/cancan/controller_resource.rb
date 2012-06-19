ryanb/cancan

commit d5baed6281b4628071f8737090eaaec5168eacbf

Fixes Nested Resource Loading

lib/cancan/controller_resource.rb
@@ -215,8 +215,12 @@ module CanCan
       end
     end
 
+    def namespace
+      @params[:controller].split("::")[0..-2]
+    end
+
     def namespaced_name
-      @name || @params[:controller].sub("Controller", "").singularize.camelize.constantize
+      [namespace, name.camelize].join('::').singularize.camelize.constantize
     rescue NameError
       name
     end

spec/cancan/controller_resource_spec.rb
@@ -215,6 +215,14 @@ describe CanCan::ControllerResource do
     resource.should_not be_parent
   end
 
+  it "should have the specified resource_class if 'name' is passed to load_resource" do
+    class Section
+    end
+
+    resource = CanCan::ControllerResource.new(@controller, :section)
+    resource.send(:resource_class).should == Section
+  end
+
   it "should load parent resource through proper id parameter" do
     project = Project.create!
     @params.merge!(:controller => "categories", :action => "index", :project_id => project.id)

commit 29a2d58ae1362720658b14774f34f63a23d36fea

Fix broken spec and had to undo the changes introduced in https://github.com/ryanb/cancan/commit/d5baed6281b4628071f8737090eaaec5168eacbf. Haven't really investigated why this code fixes the bug that commit fixed already.

lib/cancan/controller_resource.rb
@@ -236,12 +236,8 @@ module CanCan
       @name || name_from_controller
     end
 
-    def namespace
-      @params[:controller].split("::")[0..-2]
-    end
-
     def namespaced_name
-      [namespace, name.camelize].join('::').singularize.camelize.constantize
+      @name || @params[:controller].sub("Controller", "").singularize.camelize.constantize
     rescue NameError
       name
     end