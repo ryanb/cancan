2b660d7b637f4118fec3170ee76b63db1a17d5c8
Martijn Storck <martijn@lico.nl>
2012-06-19 10:40:09 +0200

    Rename :assignment option to :assign_as

--- lib/cancan/controller_additions.rb
+++ lib/cancan/controller_additions.rb
@@ -112,8 +112,8 @@ module CanCan
       # [:+prepend+]
       #   Passing +true+ will use prepend_before_filter instead of a normal before_filter.
       #
-      # [:+assignment+]
-      #   Passing to ActiveRecord for support Mass-Assignment Role
+      # [:+assign_as+]
+      #   Passed as the role when mass assigning attributes (from Rails 3.1 onwards)
       #
       def load_resource(*args)
         cancan_resource_class.add_before_filter(self, :load_resource, *args)
