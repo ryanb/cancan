857dd075c521641c3af258094b5563073e373e5a
Caleb Thompson <calebthompson89@gmail.com>
2012-08-01 17:44:21 -0300

    Refold generated Ability comments at 80 characters
    A lot of people still fold their code at 80 characters, so it is
    nice to have generated code consider this.

--- lib/generators/cancan/ability/templates/ability.rb
+++ lib/generators/cancan/ability/templates/ability.rb
@@ -11,18 +11,22 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
+    # The first argument to `can` is the action you are giving the user 
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
+    # The second argument is the resource the user can perform the action on. 
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
     #
-    # The third argument is an optional hash of conditions to further filter the objects.
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
     # For example, here the user can only update published articles.
     #
     #   can :update, Article, :published => true
     #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    # See the wiki for details:
+    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
