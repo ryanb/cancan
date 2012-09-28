0a66e445f656bc9fa687cd5e9231e3c713aab408
Delwyn de Villiers <delwyn.d@gmail.com>
2012-09-28 13:47:15 +0100

    allow be_able_to matcher to handle checks on multiple actions

--- lib/cancan/matchers.rb
+++ lib/cancan/matchers.rb
@@ -1,14 +1,35 @@
 rspec_module = defined?(RSpec::Core) ? 'RSpec' : 'Spec'  # for RSpec 1 compatability
 Kernel.const_get(rspec_module)::Matchers.define :be_able_to do |*args|
+
+  @actions = Array(args.first)
+  @errors = []
+
+  def ability_can?(ability, *args)
+    extra_args = args[1..-1]
+    @actions.each do |action|
+      @errors << action unless ability.can?(action, *extra_args)
+    end
+  end
+
   match do |ability|
-    ability.can?(*args)
+    ability_can?(ability, *args)
+    @errors.empty?
+  end
+
+  match_for_should_not do |ability|
+    ability_can?(ability, *args)
+    @errors.any?
   end
 
   failure_message_for_should do |ability|
-    "expected to be able to #{args.map(&:inspect).join(" ")}"
+    message = "expected to be able to #{args.map(&:inspect).join(" ")}"
+    message << " but was not able to #{@errors.inspect}" if args.first.kind_of?(Array)
+    message
   end
 
   failure_message_for_should_not do |ability|
-    "expected not to be able to #{args.map(&:inspect).join(" ")}"
+    message = "expected not to be able to #{args.map(&:inspect).join(" ")}"
+    message << " but was able to #{(@actions - @errors).inspect}" if args.first.kind_of?(Array)
+    message
   end
 end
